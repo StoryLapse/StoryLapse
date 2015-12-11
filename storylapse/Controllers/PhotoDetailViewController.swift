f0c419b7c1fdf141456bd25bbb8e949564c4c77b
Khuong Pham <dkhuong291@gmail.com>
2015-12-11 17:54:16 +0700

    Set Play Button for automatically switch for all photo images with fade in fade out

--- storylapse/Controllers/PhotoDetailViewController.swift
+++ storylapse/Controllers/PhotoDetailViewController.swift
@@ -12,56 +12,56 @@ import AAShareBubbles
 
 class PhotoDetailViewController: UIViewController, AAShareBubblesDelegate {
 
-    
-    @IBOutlet var topView: UIView!
-    @IBOutlet var bottomView: UIView!
-    @IBOutlet var photoImageView: UIImageView!
-    var photoImage = UIImage()
-    var checkTapGestureRecognize = true
 
-    override func viewDidLoad() {
-        
-        title = "Photo Detail"
-        super.viewDidLoad()
-        photoImageView.image = photoImage
+  @IBOutlet var topView: UIView!
+  @IBOutlet var bottomView: UIView!
+  @IBOutlet var photoImageView: UIImageView!
+  var photoImage = UIImage()
+  var checkTapGestureRecognize = true
+
+  override func viewDidLoad() {
+
+    title = "Photo Detail"
+    super.viewDidLoad()
+    photoImageView.image = photoImage
+  }
+
+  @IBAction func onBackClicked(sender: AnyObject) {
+    self.navigationController?.popViewControllerAnimated(true)
+  }
+
+
+  @IBAction func onTabGestureRecognize(sender: UITapGestureRecognizer) {
+    print("on tap")
+    if checkTapGestureRecognize == true {
+      bottomView.hidden = true
+      topView.hidden = true
+      self.navigationController?.navigationBarHidden = true
+      let screenSize: CGRect = UIScreen.mainScreen().bounds
+      let screenWidth = screenSize.width
+      let screenHeight = screenSize.height
+      photoImageView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+      checkTapGestureRecognize = false
+      showAminationOnAdvert()
     }
-
-    @IBAction func onBackClicked(sender: AnyObject) {
-        self.navigationController?.popViewControllerAnimated(true)
+    else if checkTapGestureRecognize == false {
+      bottomView.hidden = false
+      topView.hidden = false
+      self.navigationController?.navigationBarHidden = false
+      checkTapGestureRecognize = true
     }
+  }
 
-    
-    @IBAction func onTabGestureRecognize(sender: UITapGestureRecognizer) {
-        print("on tap")
-        if checkTapGestureRecognize == true {
-            bottomView.hidden = true
-            topView.hidden = true
-            self.navigationController?.navigationBarHidden = true
-            let screenSize: CGRect = UIScreen.mainScreen().bounds
-            let screenWidth = screenSize.width
-            let screenHeight = screenSize.height
-            photoImageView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
-            checkTapGestureRecognize = false
-            showAminationOnAdvert()
-        }
-        else if checkTapGestureRecognize == false {
-            bottomView.hidden = false
-            topView.hidden = false
-            self.navigationController?.navigationBarHidden = false
-            checkTapGestureRecognize = true
-        }
-    }
+  func showAminationOnAdvert() {
+    let transitionAnimation = CATransition();
+    transitionAnimation.type = kCAEmitterBehaviorValueOverLife
+    transitionAnimation.subtype = kCAEmitterBehaviorValueOverLife
+    transitionAnimation.duration = 2.5
+    transitionAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+    transitionAnimation.fillMode = kCAFillModeBoth
+    photoImageView.layer.addAnimation(transitionAnimation, forKey: "fadeAnimation")
 
-    func showAminationOnAdvert() {
-        let transitionAnimation = CATransition();
-        transitionAnimation.type = kCAEmitterBehaviorValueOverLife
-        transitionAnimation.subtype = kCAEmitterBehaviorValueOverLife
-        transitionAnimation.duration = 2.5
-        transitionAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        transitionAnimation.fillMode = kCAFillModeBoth
-        photoImageView.layer.addAnimation(transitionAnimation, forKey: "fadeAnimation")
-
-    }
+  }
 
 
   @IBAction func onShareTouched(sender: AnyObject) {
@@ -72,16 +72,41 @@ class PhotoDetailViewController: UIViewController, AAShareBubblesDelegate {
 
     let shareVC: UIActivityViewController = UIActivityViewController(activityItems: [myShare], applicationActivities: nil)
     self.presentViewController(shareVC, animated: true, completion: nil)
-//    print("share bubles")
-//    let shareBubles: AAShareBubbles = AAShareBubbles.init(centeredInWindowWithRadius: 100)
-//    shareBubles.delegate = self
-//    shareBubles.bubbleRadius = 40
-//    shareBubles.sizeToFit()
-//    //shareBubles.showFacebookBubble = true
-//    shareBubles.showTwitterBubble = true
-//    shareBubles.addCustomButtonWithIcon(UIImage(named: "twitter"), backgroundColor: UIColor.whiteColor(), andButtonId: 100)
-//    shareBubles.show()
+    //    print("share bubles")
+    //    let shareBubles: AAShareBubbles = AAShareBubbles.init(centeredInWindowWithRadius: 100)
+    //    shareBubles.delegate = self
+    //    shareBubles.bubbleRadius = 40
+    //    shareBubles.sizeToFit()
+    //    //shareBubles.showFacebookBubble = true
+    //    shareBubles.showTwitterBubble = true
+    //    shareBubles.addCustomButtonWithIcon(UIImage(named: "twitter"), backgroundColor: UIColor.whiteColor(), andButtonId: 100)
+    //    shareBubles.show()
 
   }
+
+  @IBAction func playAutomaticPhotoImages(sender: AnyObject) {
+    animateImages(0)
+  }
+
+  func animateImages(no: Int) {
+    var number: Int = no
+    if number == images.count - 1 {
+      number = 0
+    }
+    let name: String = images[number]
+    self.photoImageView!.alpha = 0.5
+    self.photoImageView!.image = UIImage(named: name)
+
+    //code to animate bg with delay 2 and after completion it recursively calling animateImage method
+    UIView.animateWithDuration(2.0, delay: 0.8, options:UIViewAnimationOptions.CurveEaseInOut, animations: {() in
+      self.photoImageView!.alpha = 1.0;
+      },
+      completion: {(Bool) in
+        number++;
+        self.animateImages(number);
+        print(String(images[number]))
+    })
+  }
+
 
 }
--- storylapse/Controllers/PhotoViewController.swift
+++ storylapse/Controllers/PhotoViewController.swift
@@ -9,77 +9,79 @@
 import UIKit
 import FXImageView
 
+public var images = ["11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18"]
+
 class PhotoViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
 
-    @IBOutlet var navigationBar: UIBarButtonItem!
-    @IBOutlet var collectionView: UICollectionView!
-    var images = [String]()
+  @IBOutlet var navigationBar: UIBarButtonItem!
+  @IBOutlet var collectionView: UICollectionView!
+  //    var images = [String]()
 
-    override func viewDidLoad() {
+  override func viewDidLoad() {
 
-        super.viewDidLoad()
-        title = "Story??"
-        navigationController!.navigationBar.barTintColor = UIColor(red: 82/255, green: 173/255, blue: 243/255, alpha: 1.0)
-        navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    super.viewDidLoad()
+    title = "Story??"
+    navigationController!.navigationBar.barTintColor = UIColor(red: 82/255, green: 173/255, blue: 243/255, alpha: 1.0)
+    navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
 
-//        images = NSMutableArray(array: ["1","2","3","4","5","6","7","8","9","10", "1","2","3","4","5","6","7","8","9","10", "1","2","3","4","5","6","7","8","9","10"])
-        images = ["11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18"]
-        self.navigationBar.image = UIImage(named: "share")?.imageWithRenderingMode(.AlwaysOriginal)
+    //        images = NSMutableArray(array: ["1","2","3","4","5","6","7","8","9","10", "1","2","3","4","5","6","7","8","9","10", "1","2","3","4","5","6","7","8","9","10"])
+    //        images = ["11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18", "11", "12", "13", "14", "15", "16", "17", "18"]
+    self.navigationBar.image = UIImage(named: "share")?.imageWithRenderingMode(.AlwaysOriginal)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // MARK: Collection
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return images.count
+  }
+
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! PhotoCollectionViewCell
+    cell.photoImageView.image = UIImage(named: images[indexPath.row])
+    cell.photoImageView.alpha = 0
+
+    let millisecondDelay = UInt64(arc4random() % 600) / 1000
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(millisecondDelay * NSEC_PER_SEC)), dispatch_get_main_queue(),({ () -> Void in
+
+      UIView.animateWithDuration(0.5, animations: ({
+        cell.photoImageView.alpha = 1.0
+      }))
+    }))
+    return cell
+  }
+
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    return CGSize(width: collectionView.frame.width/3 - 1.5, height: collectionView.frame.width/3 - 1)
+  }
+
+  func collectionView(collection: UICollectionView, selectedItemIndex: NSIndexPath) {
+    self.performSegueWithIdentifier("Detail", sender: self)
+  }
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+    if segue.identifier == "Detail" {
+      let indexPaths = self.collectionView.indexPathsForSelectedItems()!
+      let indexPath = indexPaths[0] as NSIndexPath
+      let photoDetailVC = segue.destinationViewController as! PhotoDetailViewController
+
+      photoDetailVC.photoImage = UIImage(named: images[indexPath.row])!
     }
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBAction func onShareTouched(sender: AnyObject) {
 
-    // MARK: Collection
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
+  }
 
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
-    }
 
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! PhotoCollectionViewCell
-        cell.photoImageView.image = UIImage(named: images[indexPath.row])
-        cell.photoImageView.alpha = 0
-
-        let millisecondDelay = UInt64(arc4random() % 600) / 1000
-
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(millisecondDelay * NSEC_PER_SEC)), dispatch_get_main_queue(),({ () -> Void in
-
-            UIView.animateWithDuration(0.5, animations: ({
-                cell.photoImageView.alpha = 1.0
-            }))
-        }))
-        return cell
-    }
-
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width/3 - 1.5, height: collectionView.frame.width/3 - 1)
-    }
-
-    func collectionView(collection: UICollectionView, selectedItemIndex: NSIndexPath) {
-        self.performSegueWithIdentifier("Detail", sender: self)
-    }
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-
-        if segue.identifier == "Detail" {
-            let indexPaths = self.collectionView.indexPathsForSelectedItems()!
-            let indexPath = indexPaths[0] as NSIndexPath
-            let photoDetailVC = segue.destinationViewController as! PhotoDetailViewController
-
-            photoDetailVC.photoImage = UIImage(named: images[indexPath.row])!
-        }
-    }
-
-    @IBAction func onShareTouched(sender: AnyObject) {
-
-    }
-    
-    
 }
